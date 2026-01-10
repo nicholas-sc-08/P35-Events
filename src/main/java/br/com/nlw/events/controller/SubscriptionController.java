790b5727a6605ce81784922b6e0f7ef928ea2572
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-01-10 00:45:04 -0300

    feat: ranking by user

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -3,6 +3,7 @@ package br.com.nlw.events.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,14 @@ public class SubscriptionController {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/subscription/{prettyName}/ranking")
+    public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName) {
+        try{
+            return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
+        } catch(EventNotFoundException ex) {
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
+        }
     }
 }
